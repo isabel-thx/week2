597717d45cb155c2c347496284ffe0cd6cf4ffc8
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 18:28:52 +0800

    Update fibonacci.rb

--- fibonacci.rb
+++ fibonacci.rb
@@ -9,13 +9,13 @@ end
 
 fibonacci_iterative(5)
 
-puts
 
 
 def fibonacci_recursive(n)
-  	return 0 if n == 0
-    return 1 if n == 1
-    return 1 if n == 2
-    fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
-  end
+  return 0 if n == 0
+  return 1 if n == 1
+  return 1 if n == 2
+  fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+end
+
 p fibonacci_recursive(4)
