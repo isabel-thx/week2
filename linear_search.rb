18079fd09139e747b74456b2d2ec0ae4c2ffff90
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-12 18:31:03 +0800

    Update linear_search.rb

--- linear_search.rb
+++ linear_search.rb
@@ -3,37 +3,36 @@ require "byebug"
 # put byebug where it should pause
 
 random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37]
+
 def linear_search(num, random_numbers)
-	i = 0
-	while i < random_numbers.length
-    	if random_numbers[i] == num
-    		return i
-    	end
-    	i += 1
-  	end
-  	return nil
+  i = 0
+    while i < random_numbers.length
+      if random_numbers[i] == num
+        return i
+      end
+      i += 1
+    end
+  return nil
 end
 
 p linear_search(6, random_numbers)
-
-puts
 
 
 
 bananas_arr = "bananas".split(//)
 
 def global_linear_search(letter, bananas_arr)
-	indexes_arr = []
-	a = 0
-	while a < bananas_arr.length
+  indexes_arr = []
+    a = 0
+    while a < bananas_arr.length
 	
-    	if bananas_arr[a] == letter
-    		indexes_arr << a
-    	end
-    	a += 1
-  	end
-  	return nil if indexes_arr.empty?
-	return indexes_arr
+    if bananas_arr[a] == letter
+      indexes_arr << a
+    end
+    a += 1
+    end
+  return nil if indexes_arr.empty?
+  return indexes_arr
 end
 
 p global_linear_search("a", bananas_arr)
