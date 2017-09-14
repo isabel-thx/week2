2c479757275d7de9b2e8d3fa0e4326f74ba4a63f
Isabel Teh <isabelhx.teh@gmail.com>
2017-09-14 16:50:58 +0800

    Update tic_tac_toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,16 +3,16 @@
 def tic_tac_toe(n)	
   for i in 1..n
 
-input = ["x", "o"]
-total = (["x"] * 4 + ["o"] * 4 + [input.sample]).shuffle
-array = total.each_slice(3).to_a
+	input = ["x", "o"]
+	total = (["x"] * 4 + ["o"] * 4 + [input.sample]).shuffle
+	array = total.each_slice(3).to_a
 
-puts "board #{i}"
-puts "#{array[0][0]} | #{array[0][1]} | #{array[0][2]}"
-puts "#{array[1][0]} | #{array[1][1]} | #{array[1][2]}"
-puts "#{array[2][0]} | #{array[2][1]} | #{array[2][2]}"
+	puts "board #{i}"
+	puts "#{array[0][0]} | #{array[0][1]} | #{array[0][2]}"
+	puts "#{array[1][0]} | #{array[1][1]} | #{array[1][2]}"
+	puts "#{array[2][0]} | #{array[2][1]} | #{array[2][2]}"
 	
-end
+	end
 end
 
 tic_tac_toe(10)
